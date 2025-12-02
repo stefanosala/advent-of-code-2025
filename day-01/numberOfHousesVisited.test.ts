54421a33afc30ec77fdecec0a0536479eb7e19f1
Stefano Sala <stefano@sala.io>
2025-12-02 12:29:01 +0100

    Day 01 - Second part

--- day-01/numberOfHousesVisited.test.ts
+++ day-01/numberOfHousesVisited.test.ts
@@ -2,7 +2,9 @@ import { test } from "node:test";
 import assert from "node:assert";
 import numberOfHousesVisited from "./numberOfHousesVisited.js";
 
-const input = `L68
+const CASES: [string, number][] = [
+  [
+    `L68
 L30
 R48
 L5
@@ -12,8 +14,40 @@ L1
 L99
 R14
 L82
-`;
+`,
+    6,
+  ],
+  [`L49
+L2
+`, 1],
+[`L49
+L1
+`, 1],
+[`R49
+R1
+`, 1],
+[`R49
+R2
+`, 1],
+[`R50
+R100
+`, 2],
+[`R150`, 2],
+[`R1000`, 10],
+[`R50
+L617
+`, 7]
+];
 
-test("numberOfHousesVisited", () => {
-  assert.deepEqual(numberOfHousesVisited(input).numberOfHousesVisited, 3);
+CASES.forEach(([input, expected]) => {
+  test(`numberOfHousesVisited: ${input} - ${expected}`, () => {
+    const result = numberOfHousesVisited(input);
+
+    assert.deepEqual(
+      result.numberOfHousesVisited,
+      expected
+    );
+
+    assert(result.position >= 0);
+  });
 });
--- day-01/numberOfHousesVisited.ts
+++ day-01/numberOfHousesVisited.ts
@@ -1,11 +1,38 @@
-type Step = {
+type StepResult = {
   position: number;
   numberOfHousesVisited: number;
 };
 
+const runStep = (
+  previousStepResult: StepResult,
+  direction: "L" | "R",
+  steps: number
+): StepResult => {
+  const multiplier = direction === "L" ? -1 : 1;
+
+  const newPosition = ((previousStepResult.position + steps * multiplier) % 100 + 100) % 100;
+
+  let newVisits = 0;
+
+  newVisits += Math.floor(steps / 100);
+
+  if (previousStepResult.position !== 0) {
+    if (newPosition === 0) newVisits += 1;
+    else {
+      if (direction === "R" && previousStepResult.position > newPosition) newVisits += 1;
+      if (direction === "L" && previousStepResult.position < newPosition) newVisits += 1;
+    }
+  }
+
+  return {
+    position: newPosition,
+    numberOfHousesVisited: previousStepResult.numberOfHousesVisited + newVisits,
+  };
+};
+
 export default (input: string) =>
   input.split("\n").reduce(
-    (step: Step, line) => {
+    (step: StepResult, line) => {
       const direction = line[0];
       const steps = Number(line.slice(1));
 
@@ -13,16 +40,7 @@ export default (input: string) =>
         return step;
       }
 
-      const multiplier = direction === "L" ? -1 : 1;
-
-      const newPosition =
-        (step.position + steps * multiplier + 100) % 100;
-
-      return {
-        position: newPosition,
-        numberOfHousesVisited:
-          step.numberOfHousesVisited + (newPosition === 0 ? 1 : 0),
-      };
+      return runStep(step, direction as "L" | "R", steps);
     },
     { position: 50, numberOfHousesVisited: 0 }
   );
